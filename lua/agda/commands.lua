2c19853c8c51f80778e29a8fb4d3b2a087d7616e
István Donkó <istvan.donko@gmail.com>
2022-01-10 12:44:50 +0100

    Rewrite commands with string.format

--- lua/agda/commands.lua
+++ lua/agda/commands.lua
@@ -1,53 +1,87 @@
--- TODO Rewrite these using string.format
-
 local function make (filename, command)
-  return 'IOTCM "' .. filename .. '" NonInteractive Direct (' .. command .. ')'
+  return string.format(
+    'IOTCM "%s" NonInteractive Direct (%s)',
+    filename, command
+  )
 end
 
 local function make_interval (interval)
-  return 'Interval (Pn () ' .. interval.start.pos .. ' ' .. interval.start.line .. ' ' .. interval.start.col .. ') (Pn () ' .. interval['end'].pos .. ' ' .. interval['end'].line .. ' ' .. interval['end'].col .. ')'
+  return string.format(
+    'Interval (Pn () %d %d %d) (Pn () %d %d %d)',
+    interval.start.pos, interval.start.line, interval.start.col,
+    interval['end'].pos, interval['end'].line, interval['end'].col
+  )
 end
 
 local function make_range (filename, interval)
-  return 'intervalsToRange (Just (mkAbsolute "' .. filename .. '")) [' .. interval .. ']'
-  -- return 'intervalsToRange Nothing [' .. interval .. ']'
+  return string.format(
+    -- 'intervalsToRange Nothing [%s]',
+    'intervalsToRange (Just (mkAbsolute "%s")) [%s]',
+    filename, interval
+  )
 end
 
 local function load (filename)
-  return 'Cmd_load "' .. filename .. '" []'
+  return string.format(
+    'Cmd_load "%s" []',
+    filename
+  )
 end
 
 local function version ()
-  return 'Cmd_show_version'
+  return string.format(
+    'Cmd_show_version'
+  )
 end
 
 local function auto (goal_id)
-  return 'Cmd_autoOne ' .. goal_id .. ' noRange ""'
+  return string.format(
+    'Cmd_autoOne %d noRange ""',
+    goal_id
+  )
 end
 
 local function refine (goal_id)
-  return 'Cmd_refine_or_intro False ' .. goal_id .. ' noRange ""'
+  return string.format(
+    'Cmd_refine_or_intro False %s noRange ""',
+    goal_id
+  )
 end
 
 local function case (goal_id, expression)
-  return 'Cmd_make_case ' .. goal_id .. ' noRange "' .. expression .. '"'
+  return string.format(
+    'Cmd_make_case %s noRange "%s"',
+    goal_id, expression
+  )
 end
 
 local function goal_type_context_infer (goal_id, content)
-  return 'Cmd_goal_type_context_infer Simplified ' .. goal_id .. ' noRange "' .. content .. '"'
+  return string.format(
+    'Cmd_goal_type_context_infer Simplified %s noRange "%s"',
+    goal_id, content
+  )
 end
 
 local function goal_type_context (goal_id)
-  return 'Cmd_goal_type_context Simplified ' .. goal_id .. ' noRange ""'
+  return string.format(
+    'Cmd_goal_type_context Simplified %s noRange ""',
+    goal_id
+  )
 end
 
 local function context (goal_id)
-  return 'Cmd_context Simplified ' .. goal_id .. ' noRange ""'
+  return string.format(
+    'Cmd_context Simplified %s noRange ""',
+    goal_id
+  )
 end
 
 local function give (goal_id, content, range)
-  -- return 'Cmd_give WithoutForce ' .. goal_id .. ' noRange "' .. content .. '"'
-  return 'Cmd_give WithoutForce ' .. goal_id .. ' (' .. range .. ') "' .. content .. '"'
+  return string.format(
+    -- 'Cmd_give WithoutForce %s noRange "%s"'
+    'Cmd_give WithoutForce %s (%s) "%s"',
+    goal_id, range, content
+  )
 end
 
 return ({
